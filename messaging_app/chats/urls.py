8c63b68671c7721c208518a8446fd4e823e24e72
MARWAHAMED629 <mmero385@gmail.com>
2025-06-01 02:50:09 +0200

    Fix: use NestedDefaultRouter and add api-auth

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-import rest_framework.routers as routers
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
