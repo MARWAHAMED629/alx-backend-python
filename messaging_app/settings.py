a33a533db10c1b76c0e5e6ab54b12c6c5092ff72
MARWAHAMED629 <mmero385@gmail.com>
2025-06-08 04:43:57 +0200

    add rest frame work

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,12 +1,13 @@
+import rest_framework
+
 INSTALLED_APPS = [
-   
     'rest_framework',
     'rest_framework_simplejwt',
-
 ]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
