fb14837954e61a387bdce407c544184425e851d9
MARWAHAMED629 <mmero385@gmail.com>
2025-06-08 21:44:57 +0200

    fix syntax err in middlware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,18 +1,31 @@
+import os
 from datetime import datetime
 import logging
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+log_path = os.path.join(BASE_DIR, 'requests.log')
+
 logger = logging.getLogger('request_logger')
-handler = logging.FileHandler('requests.log')
+logger.propagate = False
+
+handler = logging.FileHandler(log_path)
 formatter = logging.Formatter('%(message)s')
 handler.setFormatter(formatter)
+
+if logger.hasHandlers():
+    logger.handlers.clear()
+
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        user = request.user if request.user.is_authenticated else 'AnonymousUser'
+        user = request.user if request.user.is_authenticated else 'AnonymousUser'  # هنا غيرت الاسم
         logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
-        return self.get_response(request)
+        response = self.get_response(request)
+        return response
