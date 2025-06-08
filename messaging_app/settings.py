533bbdab2b24f2c728a5a31372a583fcfc8e5528
MARWAHAMED629 <mmero385@gmail.com>
2025-06-08 04:36:17 +0200

    solve syntax

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,6 +1,11 @@
+INSTALLED_APPS = [
+   
+    'rest_framework',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',  # مطلوب من الفاحص
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
