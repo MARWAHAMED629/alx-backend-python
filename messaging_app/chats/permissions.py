47c76009aa497219bb38435fced23807a78d65cb
MARWAHAMED629 <mmero385@gmail.com>
2025-06-08 11:27:50 +0200

    add user.is_authenticated

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,8 +1,18 @@
-from rest_framework.permissions import BasePermission
 from rest_framework import permissions
 
-class IsParticipantOfConversation(BasePermission):
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Custom permission class that allows access only to:
     
+    - Authenticated users.
+    - Participants of the conversation associated with the message object.
+    """
+
+    def has_permission(self, request, view):
+        # Check that the user is authenticated
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        user = request.user
-        return user in obj.conversation.participants.all()
+        # Grant access only if the user is a participant in the conversation
+        return request.user in obj.conversation.participants.all()
